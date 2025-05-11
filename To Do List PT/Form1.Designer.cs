73869ca77a540c99f5dc181626d8d19197d0a7bc
hulaanmokungsino12 <arisbautista55@gmail.com>
2025-05-11 20:42:45 +0800

    changes assigned

--- To Do List PT/Form1.Designer.cs
+++ To Do List PT/Form1.Designer.cs
@@ -27,82 +27,119 @@
             usernameTextBox = new TextBox();
             addtolistButton = new Button();
             flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            button3 = new Button();
             button2 = new Button();
             label1 = new Label();
             label2 = new Label();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // usernameTextBox
             // 
-            usernameTextBox.BackColor = SystemColors.MenuBar;
+            usernameTextBox.BackColor = Color.White;
             usernameTextBox.BorderStyle = BorderStyle.FixedSingle;
-            usernameTextBox.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            usernameTextBox.Font = new Font("Comfortaa", 13F, FontStyle.Regular, GraphicsUnit.Point, 0);
             usernameTextBox.ForeColor = SystemColors.MenuText;
             usernameTextBox.Location = new Point(42, 169);
             usernameTextBox.Margin = new Padding(4, 5, 4, 5);
-            usernameTextBox.MaxLength = 25;
+            usernameTextBox.MaxLength = 28;
             usernameTextBox.Name = "usernameTextBox";
-            usernameTextBox.Size = new Size(275, 42);
+            usernameTextBox.Size = new Size(274, 36);
             usernameTextBox.TabIndex = 0;
             // 
             // addtolistButton
             // 
             addtolistButton.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            addtolistButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            addtolistButton.Location = new Point(356, 173);
+            addtolistButton.BackColor = Color.FromArgb(38, 166, 154);
+            addtolistButton.Font = new Font("Comfortaa", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            addtolistButton.ForeColor = Color.White;
+            addtolistButton.Location = new Point(356, 166);
             addtolistButton.Margin = new Padding(4, 5, 4, 5);
             addtolistButton.Name = "addtolistButton";
-            addtolistButton.Size = new Size(147, 33);
+            addtolistButton.Size = new Size(147, 40);
             addtolistButton.TabIndex = 1;
             addtolistButton.Text = "add to list";
-            addtolistButton.UseVisualStyleBackColor = true;
+            addtolistButton.UseVisualStyleBackColor = false;
             addtolistButton.Click += addtolist;
             // 
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             flowLayoutPanel1.AutoScroll = true;
-            flowLayoutPanel1.BackColor = SystemColors.ButtonFace;
+            flowLayoutPanel1.BackColor = Color.FromArgb(250, 250, 250);
             flowLayoutPanel1.BorderStyle = BorderStyle.FixedSingle;
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button3);
             flowLayoutPanel1.Location = new Point(44, 235);
             flowLayoutPanel1.Margin = new Padding(4, 5, 4, 5);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
             flowLayoutPanel1.Size = new Size(458, 501);
             flowLayoutPanel1.TabIndex = 3;
             // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(38, 166, 154);
+            button1.Font = new Font("Comfortaa", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Select All";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += SelectAll;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(38, 166, 154);
+            button3.Font = new Font("Comfortaa", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(128, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(131, 42);
+            button3.TabIndex = 1;
+            button3.Text = "Deselect All";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += DeselectAll;
+            // 
             // button2
             // 
             button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            button2.BackColor = Color.LightCoral;
             button2.BackgroundImageLayout = ImageLayout.Center;
             button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(220, 745);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(224, 745);
             button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
             button2.Size = new Size(96, 35);
             button2.TabIndex = 4;
             button2.Text = "Remove";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += Remove;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Stencil", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(201, 33);
+            label1.Font = new Font("Comfortaa", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(38, 50, 56);
+            label1.ImageAlign = ContentAlignment.MiddleLeft;
+            label1.Location = new Point(182, 20);
             label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(161, 63);
+            label1.Size = new Size(185, 86);
             label1.TabIndex = 5;
             label1.Text = "ABSV";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Stencil", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(180, 95);
+            label2.Font = new Font("Comfortaa", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(177, 95);
             label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(190, 38);
+            label2.Size = new Size(186, 52);
             label2.TabIndex = 6;
             label2.Text = "To Do List";
             // 
@@ -111,7 +148,7 @@
             AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            BackColor = Color.Firebrick;
+            BackColor = Color.FromArgb(255, 229, 180);
             ClientSize = new Size(536, 797);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -125,6 +162,7 @@
             MaximizeBox = false;
             Name = "Form1";
             Text = "To-Do List";
+            flowLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -137,5 +175,7 @@
         private Button button2;
         private Label label1;
         private Label label2;
+        private Button button1;
+        private Button button3;
     }
 }
--- To Do List PT/Form1.cs
+++ To Do List PT/Form1.cs
@@ -10,14 +10,16 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        //mainly used sa almost lahat ng code
         private Dictionary<string, Label> labelDictionary;
+
+        //created mainly for deleting purpose for view to users
         private Dictionary<CheckBox, Panel> panelDictionary;
+
         int num;
-        const string path = "C:\\Users\\Aris\\source\\repos\\WinFormsApp1\\TDL list.txt";
+        const string path = "C:\\Users\\Aris\\source\\repos\\To Do List PT\\TDL list.txt";
 
 
-
-        int check;
         public Form1()
         {
             InitializeComponent();
@@ -27,20 +29,25 @@ namespace WinFormsApp1
             CheckSave();
 
         }
+
         private void CheckSave()
         {
+
             List<string> list = new List<string>();
             list = File.ReadAllLines(path).ToList();
+
 
             foreach (string item in list)
             {
                 flowLayoutPanel1.Controls.Add(CreatePanel(item));
             }
+
+
             for (int i = 0; i < list.Count; i++)
             {
                 if (labelDictionary[i.ToString()].Text.EndsWith(" "))
                 {
-                    labelDictionary[i.ToString()].Font = new Font("Segoe UI", 15.75F, FontStyle.Strikeout);
+                    labelDictionary[i.ToString()].Font = new Font("Comfortaa", 10F, FontStyle.Strikeout);
                 }
             }
 
@@ -51,44 +58,53 @@ namespace WinFormsApp1
 
             if (usernameTextBox.Text != "")
             {
+
                 flowLayoutPanel1.Controls.Add(CreatePanel(usernameTextBox.Text));
                 usernameTextBox.Clear();
 
 
                 List<string> list = new List<string>();
+
+
                 foreach (var values in labelDictionary.Values)
                 {
                     list.Add(values.Text);
                 }
 
+
                 File.WriteAllLines(path, list);
             }
         }
-
 
         private void Remove(object sender, EventArgs e)
         {
 
             foreach (var checkBox in panelDictionary.Keys)
             {
+
                 if (checkBox.Checked)
                 {
+                    //para maremove sa view ng user
                     panelDictionary.Remove(checkBox);
                     flowLayoutPanel1.Controls.Clear();
+                    flowLayoutPanel1.Controls.Add(button1);
+                    flowLayoutPanel1.Controls.Add(button3);
+
 
                     foreach (var checkBox1 in panelDictionary.Keys)
                     {
                         flowLayoutPanel1.Controls.Add(panelDictionary[checkBox1]);
                     }
-
-                    foreach (var keys in labelDictionary.Keys)
+                    //
+                    foreach (var key in labelDictionary.Keys)
                     {
-                        if (keys.Equals(checkBox.Name))
+                        if (key.Equals(checkBox.Name))
                         {
-                            labelDictionary.Remove(keys.ToString());
+                            labelDictionary.Remove(key.ToString());
                         }
                     }
 
+                    //para to sa checksave
                     List<string> list = new List<string>();
                     foreach (var values in labelDictionary.Values)
                     {
@@ -109,7 +125,7 @@ namespace WinFormsApp1
 
             CheckBox checkBox1 = new CheckBox();
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(5, 27);
+            checkBox1.Location = new Point(5, 26);
             checkBox1.Name = name;
             checkBox1.Size = new Size(15, 14);
             checkBox1.TabIndex = 0;
@@ -118,30 +134,32 @@ namespace WinFormsApp1
 
 
             Label usernameLabel1 = new Label();
-            usernameLabel1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular);
+            usernameLabel1.Font = new Font("Comfortaa", 10F, FontStyle.Regular);
             usernameLabel1.AutoSize = true;
-            usernameLabel1.Location = new Point(25, 10);
+            usernameLabel1.Location = new Point(25, 19);
             usernameLabel1.Name = name;
             usernameLabel1.Size = new Size(3, 3);
             usernameLabel1.TabIndex = 2;
             usernameLabel1.Text = text;
 
-            Button button1 = new Button();
-            button1.Location = new Point(350, 19);
-            button1.Name = name;
-            button1.Size = new Size(83, 31);
-            button1.TabIndex = 3;
-            button1.Text = "done";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += Done;
+            Button DoneButton = new Button();
+            DoneButton.Location = new Point(350, 18);
+            DoneButton.Name = name;
+            DoneButton.Size = new Size(83, 35);
+            DoneButton.TabIndex = 3;
+            DoneButton.BackColor = Color.FromArgb(38, 139, 87);
+            DoneButton.ForeColor = Color.White;
+            DoneButton.Text = "done";
+            DoneButton.UseVisualStyleBackColor = false;
+            DoneButton.Click += Done;
 
             Panel panel = new Panel();
-            panel.Controls.Add(button1);
+            panel.Controls.Add(DoneButton);
             panel.Controls.Add(usernameLabel1);
             panel.Controls.Add(checkBox1);
             panel.Location = new Point(3, 3);
             panel.Name = name;
-            panel.Size = new Size(490, 60);
+            panel.Size = new Size(440, 60);
             panel.TabIndex = 0;
             panel.Visible = true;
 
@@ -153,6 +171,7 @@ namespace WinFormsApp1
 
             return panel;
         }
+
         private void Done(object sender, EventArgs e)
         {
             Button button = sender as Button;
@@ -160,7 +179,7 @@ namespace WinFormsApp1
 
             if (labelDictionary[button.Name].Font.Strikeout == false)
             {
-                labelDictionary[button.Name].Font = new Font("Segoe UI", 15.75F, FontStyle.Strikeout);
+                labelDictionary[button.Name].Font = new Font("Comfortaa", 10F, FontStyle.Strikeout);
 
                 foreach (var key in labelDictionary.Keys)
                 {
@@ -170,6 +189,7 @@ namespace WinFormsApp1
                     }
 
                     List<string> list = new List<string>();
+
                     foreach (var values in labelDictionary.Values)
                     {
                         list.Add(values.Text);
@@ -180,7 +200,7 @@ namespace WinFormsApp1
             }
             else
             {
-                labelDictionary[button.Name].Font = new Font("Segoe UI", 15.75F, FontStyle.Regular);
+                labelDictionary[button.Name].Font = new Font("Comfortaa", 10F, FontStyle.Regular);
 
                 foreach (var key in labelDictionary.Keys)
                 {
@@ -199,5 +219,22 @@ namespace WinFormsApp1
                 }
             }
         }
+
+        private void SelectAll(object sender, EventArgs e)
+        {
+            foreach (var CheckBox1 in panelDictionary.Keys)
+            {
+                CheckBox1.Checked = true;
+            }
+        }
+        private void DeselectAll(object sender, EventArgs e)
+        {
+            foreach (var checkbox in panelDictionary.Keys)
+            {
+                checkbox.Checked = false;
+            }
+        }
     }
 }
+//Group 4 To Do List
+//Albano, Johnmharc | Bautista, Aris | Sablaon, Andrei | Villacorte, Alexchristian
